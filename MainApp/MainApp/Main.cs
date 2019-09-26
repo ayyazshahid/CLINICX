e1f5e236197fb0b7062441908de64588648d081a
ayyaz <ayyazonline@gmail.com>
2019-09-27 01:41:29 +0500

    main

--- MainApp/MainApp/Main.cs
+++ MainApp/MainApp/Main.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace MainApp
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
